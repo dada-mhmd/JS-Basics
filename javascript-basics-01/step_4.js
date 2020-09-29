28f5149b38bd50cda70f00e8d705bab175a19030
mohammad <moudi.ld@hotmail.com>
2020-09-29 11:18:51 +0300

    step 4 finished

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("what's your name ?");
+var surname = prompt("what's your surname ?");
+var city = prompt("where do you live ?");
+
+alert("Hello " + name + " " + surname + " you live in " + city)
