7e7bb342726a6582b51a556cfa539edb209fa9b9
mohammad <moudi.ld@hotmail.com>
2020-10-01 10:08:19 +0300

    step 2 updated

--- javascript-basics-03/step 2/step_2.js
+++ javascript-basics-03/step 2/step_2.js
@@ -1,14 +1,13 @@
 let show = document.getElementById('show');
 let hide = document.getElementById('hide');
+let txt = document.getElementById('texte')
 
-function showHide() {
     show.addEventListener('click', function(){
-        this.style.display = "block";
+        txt.style.display = "block";
     })
 
     hide.addEventListener('click', function() {
-        this.style.display = "none";
+        txt.style.display = "none";
     })
-}
 
-showHide();
+
